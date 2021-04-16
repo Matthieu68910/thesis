d93397eb768c33592ce3fe89a41ee5c6141ee2ea
Matthieu <duflotmatthieu1@gmail.com>
2021-04-16 12:44:03 +0200

    16 avril 12h43

--- Root/fig17.C
+++ Root/fig17.C
@@ -151,8 +151,8 @@ bool CBC2(
 }
 
 void CopyFile(int& n){
-    string cmd = ".!rootcp -r /media/matthieu/ssd1/Geant4/Data/test_" + std::to_string(n);
-    cmd += ".root /media/matthieu/ssd1/Geant4/Data/test_";
+    string cmd = ".!rootcp -r /media/matthieu/ssd1/Geant4/Data/fig17/test_" + std::to_string(n);
+    cmd += ".root /media/matthieu/ssd1/Geant4/Data/fig17/test_";
     cmd += std::to_string(n);
     cmd += "_new.root";      
     char const *pchar = cmd.c_str();
@@ -160,24 +160,124 @@ void CopyFile(int& n){
 }
 
 void fig17() {
-	int n = 31; // number of files
-    // loop on all files
-    for (int i = 0; i < n; ++i)
-    {
-        CopyFile(i);
+	const Int_t n = 25; // adam2020
+ 
+	Double_t x1[n] = {	1.300,
+						1.500,
+						1.600,
+						1.800,
+						2.000,
+						2.700,
+						3.100,
+						3.100,
+						3.800,
+						4.168,
+						4.600,
+						6.200,
+						6.600,
+						7.000,
+						7.800,
+						8.100,
+						9.200,
+						9.900,
+						10.200,
+						10.500,
+						11.000,
+						11.500,
+						12.500,
+						13.300,
+						15.000};
+    Double_t y1[n] = {	1.11851,
+						1.12435,
+						1.12561,
+						1.12936,
+						1.13301,
+						1.14403,
+						1.15330,
+						1.15349,
+						1.17478,
+						1.18285,
+						1.19106,
+						1.22892,
+						1.24545,
+						1.25452,
+						1.27972,
+						1.29204,
+						1.32176,
+						1.33246,
+						1.34754,
+						1.35009,
+						1.37482,
+						1.38456,
+						1.41346,
+						1.44631,
+						1.50565};
+	Double_t ex1[n] = {0.};
+    Double_t ey1[n] = {0.};
+    					
+	const Int_t k = 31; // simulation
+ 
+	Double_t x2[k] = {	0.0,
+						0.5,
+						1.0,
+						1.5,
+						2.0,
+						2.5,
+						3.0,
+						3.5,
+						4.0,
+						4.5,
+						5.0,
+						5.5,
+						6.0,
+						6.5,
+						7.0,
+						7.5,
+						8.0,
+						8.5,
+						9.0,
+						9.5,
+						10.0,
+						10.5,
+						11.0,
+						11.5,
+						12.0,
+						12.5,
+						13.0,
+						13.5,
+						14.0,
+						14.5,
+						15.0};
+    Double_t y2[k] = {0.};
+    Double_t ex2[k] = {0.};
+    Double_t ey2[k] = {0.};
 
-        string file_path = "/media/matthieu/ssd1/Geant4/Data/test_";
-        file_path += std::to_string(i);
-        file_path += "_new.root";
+    //****************** Create Histo ************************************//
+    auto c1 = new TCanvas("c1","c1",1920,1080);
+	c1->SetTitle("Mean cluster width for 2S mini-module");
+	gStyle->SetOptStat(0);
+	gPad->SetGridx(1);
+    gPad->SetGridy(1);
+
+    gPad->SetTitle("Mean cluster width for 2S mini-module");
+
+    // loop on all files
+    for (int j = 0; j < k; ++j)
+    {
+        //CopyFile(j);
+
+        string file_path = "/media/matthieu/ssd1/Geant4/Data/fig17/test_";
+        file_path += std::to_string(j);
+        file_path += ".root";
         char const *pchar = file_path.c_str();
         TFile f(pchar, "read");
-        //cout << "File " << i << " opened:" << endl;
+        //cout << "File " << j << " opened:" << endl;
 
 	    //************* variable ***************//
 	    const int NBR_STRIP = 101;
 	    const int MAX_CLUSTER_WIDTH = 3;
 	    const int CLUSTER_WINDOW = 5;
-	    const double THRESHOLD = 0.0144; // MeV -> = 4 * (1000 * 3.6 keV)
+	    const double THRESHOLD = 0.017; // MeV -> = 6 * (1000 * 3.6 keV)
 	    
 	    Int_t nbrCAT, nbrCA, nbrCBT, nbrCB; // for A and B detectors
 	    Double_t mCWAT, mCWBT, mCWA, mCWB;
@@ -280,11 +380,45 @@ void fig17() {
 	    for (int i = 0; i < 10; ++i){variance += pow((mClusWidth.at(i) - average), 2);}
 	    variance /= 9;
 	    deviation = sqrt(variance);
-	    cout << std::scientific << "File " << i << " mean cluster width:\t" << average << "\t" << deviation << endl;
+	    cout << std::scientific << "File " << j << " mean cluster width:\t" << average << "\t" << deviation << endl;
 
 	    //*********************** 
+	    y2[j] = average;
+	    ey2[j] = deviation;
 
 	    // Close file when finished
 	    f.Close();
 	}   
+	// Fill graphs
+	TGraphErrors *gr1 = new TGraphErrors(n,x1,y1,ex1,ey1);
+    gr1->SetName("gr1");
+    gr1->SetMarkerColor(2);
+    gr1->SetMarkerStyle(20);
+    gr1->SetMarkerSize(1.2);
+
+    TGraphErrors *gr2 = new TGraphErrors(k,x2,y2,ex2,ey2);
+    gr2->SetName("gr2");
+    gr2->SetMarkerColor(4);
+    gr2->SetMarkerStyle(20);
+    gr2->SetMarkerSize(1.2);
+
+    TMultiGraph *mg = new TMultiGraph();
+    mg->Add(gr1);
+    mg->Add(gr2);
+    mg->SetTitle("Mean cluster width for 2S mini-module");
+    mg->Draw("AP");
+
+    TAxis *xaxis = mg->GetXaxis();
+    TAxis *yaxis = mg->GetYaxis();
+    xaxis->SetTitle("Incident angle [deg]");
+    xaxis->SetRangeUser(0, 15);
+    yaxis->SetTitle("Mean cluster width [strip]");
+
+    auto legend = new TLegend(0.1,0.7,0.35,0.9);
+    legend->SetHeader("Legend","C");
+    legend->AddEntry("gr1","Adam et all. 2020","p");
+    legend->AddEntry("gr2","Geant4","ep");
+    legend->Draw();
+
+    gPad->Modified();
 }
--- Root/fig20_3.C
+++ Root/fig20_3.C
@@ -7,9 +7,9 @@ bool CBC2(
 	const vector<double> &strip_B, 
 	vector<double> &res_A, // [0:4]-> 1-5 strip wide clusters,[5]-> number of clusters (tot), [6]-> mean cluster width (tot), [7:8]-> accepted
 	vector<double> &res_B,
-	const int MAX_CLUSTER_WIDTH = 3,
+	const int MAX_CLUSTER_WIDTH = 2,
 	const int CLUSTER_WINDOW = 5,
-	const double THRESHOLD = 0.0144
+	const double THRESHOLD = 0.0216
 	){
 
 	const int NBR_STRIP = strip_A.size();
@@ -159,21 +159,87 @@ void CopyFile(int& n){
     gROOT->ProcessLine(pchar);
 }
 
-void fig20() {
+void fig20_3() {
+	// data for Adam2020
+	const Int_t n = 24;
+ 
+	Double_t x1[n] = {	1.32089,
+						1.48578,
+						1.57853,
+						1.71422,
+						1.79152,
+						1.87568,
+						1.93064,
+						1.98904,
+						2.13847,
+						2.42703,
+						2.51978,
+						2.80491,
+						2.97495,
+						3.1656,
+						4.26316,
+						4.66851,
+						5.15975,
+						6.32257,
+						7.25867,
+						9.79902,
+						10.888,
+						12.2483,
+						13.0642,
+						15.0738};
+
+    Double_t y1[n] = {	0.00162849,
+						0.00298557,
+						0.00298557,
+						0.02605590,
+						0.24183200,
+						0.49832000,
+						0.66659800,
+						0.81587700,
+						0.97872600,
+						0.98279800,
+						0.98144100,
+						0.98415500,
+						0.98279800,
+						0.98415500,
+						0.98415500,
+						0.98279800,
+						0.98415500,
+						0.98415500,
+						0.98415500,
+						0.98279800,
+						0.98415500,
+						0.98415500,
+						0.98415500,
+						0.98551200};
+
+    Double_t ex1[n] = {0.};
+    Double_t ey1[n] = {0.};
+
 	// open file
-    TFile *f = TFile::Open("/media/matthieu/ssd1/Geant4/Data/data.root", "read");
+    TFile *f = TFile::Open("/media/matthieu/ssd1/Geant4/Data/data100M.root", "read");
 
     //************* variable ***************//
     const int NBR_STRIP = 101;
-    const int MAX_CLUSTER_WIDTH = 3;
-    const int CLUSTER_WINDOW = 5;
-    const double THRESHOLD = 0.0144; // MeV -> = 4 * (1000 * 3.6 keV)
-    const int NBR_BINS = 1000;
+
+    const int MAX_CLUSTER_WIDTH1 = 2;
+    const int CLUSTER_WINDOW1 =5;
+    const double THRESHOLD1 = 0.000001; // MeV -> = 0 * (1000 * 3.6 keV)
+
+    const int MAX_CLUSTER_WIDTH2 = 2;
+    const int CLUSTER_WINDOW2 =5;
+    const double THRESHOLD2 = 0.0216; // MeV -> = 6 * (1000 * 3.6 keV)
+
+    const int MAX_CLUSTER_WIDTH3 = 2;
+    const int CLUSTER_WINDOW3 =5;
+    const double THRESHOLD3 = 0.0432; // MeV -> = 12 * (1000 * 3.6 keV)
+
+    const int NBR_BINS = 500;
     
-    Int_t nbrCAT, nbrCA, nbrCBT, nbrCB; // for A and B detectors
+    /*Int_t nbrCAT, nbrCA, nbrCBT, nbrCB; // for A and B detectors
     Double_t mCWAT, mCWBT, mCWA, mCWB;
     Int_t CA1, CA2, CA3, CA4, CA5;
-    Int_t CB1, CB2, CB3, CB4, CB5;
+    Int_t CB1, CB2, CB3, CB4, CB5;*/
     Double_t momentum;
 
     Bool_t corr_stub = false; // for general
@@ -206,17 +272,31 @@ void fig20() {
     data->SetBranchAddress("momentum", &momentum);
 
     //****************** Create Histo ************************************//
-    //TCanvas *c1 = new TCanvas("c1");
-    TH1* h1 = new TH1D("h1", "Stub efficiency", NBR_BINS, 1.0, 3.5);
+    auto c1 = new TCanvas("c1","c1",1920,1080);
+	c1->SetTitle("Stub efficiency for 2S mini-module");
+	gStyle->SetOptStat(0);
+	gPad->SetGridx(1);
+    gPad->SetGridy(1);
+
+    TH1* h1 = new TH1D("h1", "Stub efficiency for 2S mini-module", NBR_BINS, 1.0, 3.5);
+    h1->SetName("h1");
+    TH1* h2 = new TH1D("h2", "Stub efficiency 2", NBR_BINS, 1.0, 3.5);
+    h2->SetName("h2");
+    TH1* h3 = new TH1D("h3", "Stub efficiency 3", NBR_BINS, 1.0, 3.5);
+    h3->SetName("h3");
+
+    gPad->SetTitle("Stub efficiency for 2S mini-module");
+
     // Create vectors for bin content
-    std::vector<double> nbr_stub(NBR_BINS+2, 0);
-    std::vector<double> nbr_event(NBR_BINS+2, 0);
+    std::vector<double> nbr_stub1(NBR_BINS+2, 0);
+    std::vector<double> nbr_event1(NBR_BINS+2, 0);
+    std::vector<double> nbr_stub2(NBR_BINS+2, 0);
+    std::vector<double> nbr_event2(NBR_BINS+2, 0);
+    std::vector<double> nbr_stub3(NBR_BINS+2, 0);
+    std::vector<double> nbr_event3(NBR_BINS+2, 0);
 
     //****************** Main loop over all entries **********************//
     int count_loop = 0;
-    bool stop = true;
-    double mean_cluster_width = 0.;
-    std::vector<double> mClusWidth;
     for (int k = 0; k < ENTRIES; k++)
     {
         // fill variables with datas from entry i
@@ -225,9 +305,12 @@ void fig20() {
         std::vector<double> res_A(9, 0);
         std::vector<double> res_B(9, 0);
 
-        bool stub = CBC2(strip_A, strip_B, res_A, res_B, MAX_CLUSTER_WIDTH, CLUSTER_WINDOW, THRESHOLD);
+        bool stub1 = CBC2(strip_A, strip_B, res_A, res_B, MAX_CLUSTER_WIDTH1, CLUSTER_WINDOW1, THRESHOLD1);
+        bool stub2 = CBC2(strip_A, strip_B, res_A, res_B, MAX_CLUSTER_WIDTH2, CLUSTER_WINDOW2, THRESHOLD2);
+        bool stub3 = CBC2(strip_A, strip_B, res_A, res_B, MAX_CLUSTER_WIDTH3, CLUSTER_WINDOW3, THRESHOLD3);
 
-        CA1 = (int)res_A.at(0); // for A
+
+        /*CA1 = (int)res_A.at(0); // for A
         CA2 = (int)res_A.at(1);
         CA3 = (int)res_A.at(2);
         CA4 = (int)res_A.at(3);
@@ -245,22 +328,86 @@ void fig20() {
         nbrCBT = (int)res_B.at(5);
         mCWBT = (double)res_B.at(6);
         nbrCB = (int)res_B.at(7);
-        mCWB = (double)res_B.at(8);
+        mCWB = (double)res_B.at(8);*/
 
-        corr_stub = stub; // for general
-        if(stub){nbr_stub.at(h1->FindBin(momentum)) += 1.;}
-        nbr_event.at(h1->FindBin(momentum)) += 1.;
+        //corr_stub = stub; // for general
+        if(stub1){nbr_stub1.at(h1->FindBin(momentum)) += 1.;}
+        nbr_event1.at(h1->FindBin(momentum)) += 1.;
+
+        if(stub2){nbr_stub2.at(h2->FindBin(momentum)) += 1.;}
+        nbr_event2.at(h2->FindBin(momentum)) += 1.;
+
+        if(stub3){nbr_stub3.at(h3->FindBin(momentum)) += 1.;}
+        nbr_event3.at(h3->FindBin(momentum)) += 1.;
+
+        count_loop += 1;
+        if (count_loop == ENTRIES / 10)
+        {
+        	count_loop = 0;
+        	cout << "*" << endl;
+        }
     }
     double error = 0., content = 0.;
     for (int i = 1; i <= NBR_BINS; ++i)
     {
-    	content = nbr_stub.at(i) / nbr_event.at(i);
+    	content = nbr_stub1.at(i) / nbr_event1.at(i);
     	h1->SetBinContent(i, content);
-    	error = sqrt(nbr_event.at(i)) / nbr_event.at(i); //* content;
-    	h1->SetBinError(i, error);
+    	//error = sqrt(nbr_event1.at(i)) / nbr_event1.at(i); //* content;
+    	//h1->SetBinError(i, error);
+    	content = nbr_stub2.at(i) / nbr_event2.at(i);
+    	h2->SetBinContent(i, content);
+    	content = nbr_stub3.at(i) / nbr_event3.at(i);
+    	h3->SetBinContent(i, content);
     }
     
-    h1->Draw();
+    h1->SetLineColor(kGreen+2);
+    h1->SetLineWidth(1);
+    //h1->SetLineStyle(9);
+    h1->Draw("SAME");
+
+    h2->SetLineColor(kBlue+2);
+    h2->SetLineWidth(1);
+    //h1->SetLineStyle(9);
+    h2->Draw("SAME");
+
+    h3->SetLineColor(kMagenta+2);
+    h3->SetLineWidth(1);
+    //h1->SetLineStyle(9);
+    h3->Draw("SAME");
+
+    TGraphErrors *gr1 = new TGraphErrors(n,x1,y1,ex1,ey1);
+    gr1->SetName("gr1");
+    gr1->SetMarkerColor(kRed+2);
+    gr1->SetMarkerStyle(20);
+    gr1->SetMarkerSize(1.2);
+    gr1->Draw("SAME P");
+
+    TF1* func1 = new TF1("func1", "([0]/(1+ TMath::Exp(-[1]*(x-[2]))))", x1[0], x1[9]);
+    func1->SetParameters(1, 15, 2);
+    func1->SetLineColor(kRed+2);
+    func1->SetLineWidth(1);
+    func1->SetLineStyle(7);
+    gr1->Fit(func1);
+
+    TAxis *xaxis = h1->GetXaxis();
+    TAxis *yaxis = h1->GetYaxis();
+    xaxis->SetTitle("Emulated pT [GeV]");
+    //xaxis->Set(25, 1.0, 3.5);
+    xaxis->SetRangeUser(1.0, 3.5);
+
+    yaxis->SetTitle("Stub efficiency");
+    yaxis->SetRangeUser(0., 1.1);
+
+    c1->RedrawAxis();
+
+    auto legend = new TLegend(0.65,0.1,0.9,0.35);
+    legend->AddEntry("gr1","Adam et al. 2020","p");
+    legend->AddEntry("h1","Geant4: threshold =  0.0 keV","l");
+    legend->AddEntry("h2","Geant4: threshold = 21.6 keV","l");
+    legend->AddEntry("h3","Geant4: threshold = 43.2 keV","l");
+    legend->Draw();
+
+    gPad->Modified();
     //*********************** 
 
     // Close file when finished
